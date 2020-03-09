c6e947bc7b3633da8ac0f096b8ba08a809420c4b
Nashir <nashiruddin.abubakar@gmail.com>
2020-03-09 13:11:41 +0700

    added model and application class, store hardcoded user into hawk

--- app/src/main/java/nash/ir/chataja/ChatAjaApp.kt
+++ app/src/main/java/nash/ir/chataja/ChatAjaApp.kt
@@ -1,2 +1,31 @@
 package nash.ir.chataja
 
+import android.app.Application
+import com.orhanobut.hawk.Hawk
+import nash.ir.chataja.model.User
+import nash.ir.chataja.util.KEY_USER_LIST
+
+class ChatAjaApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Hawk.init(this).build()
+        val userList= ArrayList<User>()
+        val user1= User(
+            "Jarjit Singh",
+            "https://api.adorable.io/avatars/160/jarjit@mail.com.png ",
+            "jarjit@mail.com",
+            "123456"
+        )
+
+        val user2= User(
+            "Ismail bin Mail",
+            "https://api.adorable.io/avatars/160/ismail@mail.com.png",
+            "ismail@mail.com",
+            "123456"
+        )
+
+        userList.add(user1)
+        userList.add(user2)
+        Hawk.put(KEY_USER_LIST,userList)
+    }
+}
--- app/src/main/java/nash/ir/chataja/model/User.kt
+++ app/src/main/java/nash/ir/chataja/model/User.kt
@@ -1,2 +1,8 @@
 package nash.ir.chataja.model
 
+data class User (
+ val name:String,
+ val avatar:String,
+ val email:String,
+ val password:String
+)
